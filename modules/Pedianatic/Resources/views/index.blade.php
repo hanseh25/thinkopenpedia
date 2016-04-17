259343cce9dfe6386057fcd05cf591db21bf8de9
Hans Christian Torres <hanseh@hanseh.local>
2016-04-17 09:27:54 +0800

    added basic analytics

--- modules/Pedianatic/Resources/views/index.blade.php
+++ modules/Pedianatic/Resources/views/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="box-body no-padding">
                   <!-- THE Analytics -->
                   <div id="pedianatic">
-                    Pedianatic
+                    <script id="infogram_0_qWPmLcxd7zVqlI8s" title="malnutrition" src="//e.infogr.am/js/embed.js?1aJ" type="text/javascript"></script><div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px"><a target="_blank" href="https://infogr.am/qWPmLcxd7zVqlI8s" style="color:#989898!important;text-decoration:none!important;">malnutrition</a><br></div>
                   </div>
                 </div><!-- /.box-body -->
               </div><!-- /. box -->
