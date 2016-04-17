17819b6bbddeb20df81319dab618a7189a5b6060
Hans Christian Torres <hanseh@hanseh.local>
2016-04-17 11:31:20 +0800

    update

--- modules/Pedianatic/Resources/views/index.blade.php
+++ modules/Pedianatic/Resources/views/index.blade.php
@@ -6,10 +6,24 @@
           <div class="row">
             <div class="col-md-12">
               <div class="box box-primary">
+                  <div class="box-header with-border">
+
+                    <div class="box-tools pull-right">
+                      <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                      </button>
+                    </div>
+                  </div>
                 <div class="box-body no-padding">
                   <!-- THE Analytics -->
                   <div id="pedianatic">
+
+                    <script id="infogram_0_UE41eqwGAqQUgLRD" title="thinkopen01" src="//e.infogr.am/js/embed.js?rde" type="text/javascript"></script><div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px"><a target="_blank" href="https://infogr.am/UE41eqwGAqQUgLRD" style="color:#989898!important;text-decoration:none!important;">thinkopen01</a><br></div>
+
+
                     <script id="infogram_0_qWPmLcxd7zVqlI8s" title="malnutrition" src="//e.infogr.am/js/embed.js?1aJ" type="text/javascript"></script><div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px"><a target="_blank" href="https://infogr.am/qWPmLcxd7zVqlI8s" style="color:#989898!important;text-decoration:none!important;">malnutrition</a><br></div>
+
+                   
+                    <script id="infogram_0_oj5RqEtNTcelbZYu" title="zika" src="//e.infogr.am/js/embed.js?s0R" type="text/javascript"></script><div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px"><a target="_blank" href="https://infogr.am/oj5RqEtNTcelbZYu" style="color:#989898!important;text-decoration:none!important;">zika</a></div>
                   </div>
                 </div><!-- /.box-body -->
               </div><!-- /. box -->
